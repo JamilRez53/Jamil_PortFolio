1d5bd5e3ecf88261266fbe18cef924bdeaa6c60f
JamilRez53 <jamilurreza53@gmail.com>
2024-01-02 22:25:30 +0600

    Push

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import {HiArrowNarrowRight} from 'react-icons/hi';
+import PortFolio from '../assets/IMG_7719.jpg';
 const Home = () => {
     const texts = [
         {
@@ -16,18 +17,32 @@ const Home = () => {
         }
     ]
   return (
-    <div className='w-full h-screen bg-[#000119]'>
-        <div className='w-full mt-[250px] px-[50px]'>
+    <div className='w-full h-screen flex flex-col-reverse md:flex-row  bg-[#000119]'>
+        <div className='mt-[250px] px-[20px] sm:mt-[200px]px-[50px]'>
+
         {texts.map((items,index)=>{
         return(
-            <div className='w-5/6 justify-around'>
-                {(index=== 2||index===3)?<p className="text-[#cccccc] text-2xl sm:text-5xl font-Kanit">{items.text}</p>:<p className='text-white font-Kanit text-4xl sm:text-7xl'>{items.text}</p>}
+            <div className='justify-between'>
+                <p className={(index===2||index===3)?"text-[#cccccc] w-7/8 sm:w-5/6 text-2xl sm:text-5xl font-Kanit":'text-white font-Kanit text-4xl sm:text-7xl'}>{items.text}</p>
             </div>
         )
      })}
+        
+     <div className="mt-4">
+     <button className='flex group font-Kanit border-2 px-6 py-2 my-2 items-center shadow-white hover:shadow-blue-800 shadow-lg bg-white hover:bg-blue-800 hover:border-blue-800 hover:text-white'>
+     View Projects
+        <span className="group-hover:rotate-90 duration-300">
+         <HiArrowNarrowRight className='ml-3'/>
+        </span>
+        </button>
+     </div>
+       
         </div>
-       
-       
+        <img
+        src={PortFolio}
+        className="m-4 sm:mt-[250px] ml-[10px] sm:w-[350px] h-[350px] sm:ml-auto sm:mr-[150px] order-first sm:order-last"
+        alt="Portfolio"
+      />
     
     </div>
   )
